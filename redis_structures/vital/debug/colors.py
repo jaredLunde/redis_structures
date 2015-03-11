cf78affa9684d54abbc65cd919973b2f1647ca71
Jared Lunde <jared.lunde@gmail.com>
2015-03-11 14:33:53 -0500

    debug

---
+++ redis_structures/vital/debug/colors.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3 -S
+# -*- coding: utf-8 -*-
+"""
+
+  SIMPLE SHELL COLORS :: COLOR WRAPPERS FOR SHELL MESSAGES
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  The MIT License (MIT)
+
+Copyright (c) 2014 Jared Lunde
+http://github.com/jaredlunde
+
+"""
+# Standard colors
+GREEN='\033[0;32m'
+BOLDGREEN='\033[1;32m'
+RED='\033[0;31m'
+BOLDRED='\033[1;31m'
+CYAN='\033[0;36m'
+BOLDCYAN='\033[1;36m'
+BLUE='\033[0;34m'
+BOLDBLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+BOLDPURPLE='\033[1;35m'
+MAGENTA='\033[0;35m'
+BOLDMAGENTA='\033[1;35m'
+YELLOW='\033[0;33m'
+GRAY='\033[0;30m'
+BOLDGRAY='\033[1;30m'
+BOLDWHITE='\033[1;37m'
+BOLD="\033[1m"
+# Reset color
+RESET='\033[1;m'
+
+# Predefined color terminology
+success_color = "GREEN"
+complete_color = "BOLDGREEN"
+notice_color = "CYAN"
+warning_color = "YELLOW"
+error_color = "BOLDRED"
+timing_color = "PURPLE"
+obj_color = "PURPLE"
+index_color = "BLUE"
+key_color = "BOLDWHITE"
+bold = "BOLD"
