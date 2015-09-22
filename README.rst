fbf89c750569b1082eef2247ca18de4341c6b52e
Jared Lunde <jared.lunde@gmail.com>
2015-09-22 17:59:36 -0500

    setup.py fixes, README.rst

--- README.rst
+++ README.rst
@@ -1,8 +1,11 @@
+===============================
 Redis Structures
-========
+===============================
 
 Redis data structures wrapped with Python 3.
-Full documentation can be found at http://docr.it/redis_structures
+
+`Full documentation can be found at http://docr.it/redis_structures
+<http://docr.it/redis_structures>`_
 
 .. image:: https://travis-ci.org/jaredlunde/redis_structures.svg?branch=master
    :target: https://travis-ci.org/jaredlunde/redis_structures
@@ -36,9 +39,10 @@ Getting Started
     >>> from redis_structures import StrictRedis, RedisHash
     >>> client = StrictRedis(host='localhost', port=6379, db=0)
     >>> rh = RedisHash("my_hash", prefix="rs:hash" client=StrictRedis)
-    >>> rh['hello'] = "world"  # sets the field name 'hello' to value 'world in
+    >>> rh['hello'] = "world"  # sets the field name 'hello' to value 'world' in
     >>>                        # redis under the key rs:hash:my_hash
     >>> rh['hello']
     'world'
-    >>> rh['hello']
-    'world'
+    >>> del rh['hello']
+    >>> rh.get('hello')
+    None
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3 -S
 import os
 import uuid
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 from pip.req import parse_requirements
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements(
@@ -12,10 +19,22 @@ install_reqs = parse_requirements(
 
 setup(
     name='redis_structures',
-    version='0.0.2',
+    version='0.1.1',
+    license='MIT',
     description='Redis data structures wrapped with Python 3.',
+    long_description=readme(),
     author='Jared Lunde',
     author_email='jared.lunde@gmail.com',
     url='https://github.com/jaredlunde/redis_structures',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=["redis", "data structures"],
     install_requires=[str(ir.req) for ir in install_reqs],
     packages=['redis_structures', 'redis_structures.debug'])
