4c9a0548ed7789a295873387244e6ddf63b5f113
Jared Lunde <jared.lunde@gmail.com>
2015-09-22 14:16:51 -0500

    travis

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,3 +7,16 @@
    http://github.com/jaredlunde
 
 """
+from .redis_defaultdict import *
+from .redis_defaulthash import *
+from .redis_dict import *
+from .redis_hash import *
+from .redis_list import *
+from .redis_map import *
+from .redis_set import *
+from .redis_sorted_set import *
+
+
+if __name__ == '__main__':
+    #: python3 ~/git/redis_structures/tests/all.py -v
+    unittest.main()
